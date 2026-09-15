677bc11707624e8cd899fcadb25d542f2b66faed
12Cchris <jjuyaho12@naver.com>
2026-09-15 18:41:38 +0900

    Update 2026-09-15 18:41:38.87

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 echo ================================================
-echo      Portfolio Site Deploy  (deploy.bat v2.0)
+echo      Portfolio Site Deploy  (deploy.bat v2.1)
 echo ================================================
 echo.
 echo This syncs commits made from the website ADMIN page
